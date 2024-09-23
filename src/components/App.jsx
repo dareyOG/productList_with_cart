1512c061a26869aa70fae19ec50a9adab8e58e6d
Oludire Damilare <oludire@gmail.com>
2024-09-23 11:34:17 +0100

    Add_To_Cart btn Episode-3 (on increase & decrease in dessert quantity)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,6 +19,46 @@ function reducer(state, action) {
         cartList: [...state.cartList, action.payload],
       };
 
+    case 'remove_dessert':
+      return {
+        ...state,
+        cartList: state.cartList.filter(
+          (cartItem) => cartItem.name.toLowerCase() !== action.payload.name.toLowerCase()
+        ),
+      };
+
+    case 'increase_count': {
+      const updatedCart = state.cartList.slice().map((cartItem) => {
+        return cartItem.name.toLowerCase() === action.payload.name.toLowerCase()
+          ? {
+              ...cartItem,
+              quantity: cartItem.quantity + 1,
+              totalPrice: cartItem.unitPrice * (cartItem.quantity + 1),
+            }
+          : cartItem;
+      });
+      return { ...state, cartList: updatedCart };
+    }
+
+    case 'decrease_count': {
+      const updatedCart = state.cartList
+        .slice()
+        .map((cartItem) => {
+          return cartItem.name.toLowerCase() === action.payload.name.toLowerCase()
+            ? cartItem.quantity > 1
+              ? {
+                  ...cartItem,
+                  quantity: cartItem.quantity - 1,
+                  totalPrice: cartItem.unitPrice * (cartItem.quantity - 1),
+                }
+              : null
+            : { ...cartItem };
+        })
+        .filter((cartItem) => cartItem !== null);
+
+      return { ...state, cartList: updatedCart };
+    }
+
     default:
       throw new Error('unknown action');
   }
