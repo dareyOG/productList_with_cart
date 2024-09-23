14e9ff271c999c14be1971b81e455471e1afdae8
Oludire Damilare <oludire@gmail.com>
2024-09-23 11:07:28 +0100

    log dessert data to console

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useReducer, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialState = {
+  desserts: [],
+  cartList: [],
+  isModalActive: false,
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function reducer(state, action) {
+  switch (action.type) {
+    case 'render_desserts':
+      return { ...state, desserts: action.payload };
+
+    default:
+      throw new Error('unknown action');
+  }
 }
 
-export default App
+function App() {
+  const [{ desserts }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function getDessert() {
+      const dessertRes = await fetch('/data/data.json');
+      const dessertData = await dessertRes.json();
+      dispatch({ type: 'render_desserts', payload: dessertData.desserts });
+    }
+    getDessert();
+  }, []);
+
+  console.log(desserts);
+
+  return;
+}
+
+export default App;
